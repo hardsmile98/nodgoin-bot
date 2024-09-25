3c264f7326a9c2cc8f0a65eb0d159f12769ea19c
Denis Kotelev <d.kotelev@carprice.tech>
2024-09-25 15:50:42 +0300

    f

--- src/commands/start/checkRef.ts
+++ src/commands/start/checkRef.ts
@@ -9,45 +9,49 @@ const bonusForInvite = {
 }
 
 const checkRef = async (payload: string, user: UserEntity) => {
-  const inviterUuid = payload.split('_')?.[1]
+  try {
+    const inviterUuid = payload.split('_')?.[1]
 
-  if (user.id === inviterUuid) {
-    return
-  }
-
-  if (!Number.isInteger(+inviterUuid)) {
-    const inviter = await UserRepository.findOneBy({ id: inviterUuid })
-
-    if (!inviter) {
-      return false
+    if (user.id === inviterUuid) {
+      return
     }
 
-    const refFinded = await ReferralsRepository.findOneBy({
-      user_id: inviter.id,
-      referral_id: user.id
-    })
+    if (!Number.isInteger(+inviterUuid)) {
+      const inviter = await UserRepository.findOneBy({ id: inviterUuid })
 
-    if (refFinded) {
-      return false
+      if (!inviter) {
+        return false
+      }
+
+      const refFinded = await ReferralsRepository.findOneBy({
+        user_id: inviter.id,
+        referral_id: user.id
+      })
+
+      if (refFinded) {
+        return false
+      }
+
+      const newRef = ReferralsRepository.create({
+        user_id: inviter.id,
+        referral_id: user.id
+      })
+
+      await ReferralsRepository.save(newRef)
+
+      const bonus = bonusForInvite[inviter.ref_type]
+
+      inviter.balance = inviter.balance + bonus
+      await UserRepository.save(inviter)
+
+      user.balance = user.balance + bonus
+      user.invited = inviter.id
+      await UserRepository.save(user)
+
+      return true
     }
-
-    const newRef = ReferralsRepository.create({
-      user_id: inviter.id,
-      referral_id: user.id
-    })
-
-    await ReferralsRepository.save(newRef)
-
-    const bonus = bonusForInvite[inviter.ref_type]
-
-    inviter.balance = inviter.balance + bonus
-    await UserRepository.save(inviter)
-
-    user.balance = user.balance + bonus
-    user.invited = inviter.id
-    await UserRepository.save(user)
-
-    return true
+  } catch (e) {
+    console.log(`Error in checkRef: ${e.message}`)
   }
 }
 
--- src/commands/start/checkSquad.ts
+++ src/commands/start/checkSquad.ts
@@ -2,16 +2,20 @@ import { SquadRepository, UserRepository } from '../../db'
 import type UserEntity from '../../db/entity/user.entity'
 
 const checkSquad = async (payload: string, user: UserEntity) => {
-  const squadId = payload.split('_')?.[1]
+  try {
+    const squadId = payload.split('_')?.[1]
 
-  const squad = await SquadRepository.findOneBy({ id: squadId })
+    const squad = await SquadRepository.findOneBy({ id: squadId })
 
-  if (squad) {
-    user.squad_id = squad.id
+    if (squad) {
+      user.squad_id = squad.id
 
-    await UserRepository.save(user)
+      await UserRepository.save(user)
 
-    return squad
+      return squad
+    }
+  } catch (e) {
+    console.log(`Error in checkSquad: ${e.message}`)
   }
 }
 
